fc36819ffe723b020d03be6f7c709dac7749e6a5
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-25 01:20:30 +0000

    IncompleteCardGenerator

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,33 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  document.querySelectorAll("#symbol").classList.add(generateRandomSuite());
+  //   document.querySelector(".symbolbottom").innerHTML = generateRandomSuite();
+};
+
+let generateRandomNumber = () => {
+  let array = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
+  let indexNumbers = Math.floor(Math.random() * array.length);
+  return array[indexNumbers];
+};
+
+let generateRandomSuite = () => {
+  let suites = ["diamond", "ace", "spade", "club"];
+  let indexSuites = Math.floor(Math.random() * suites.length);
+  return suites[indexSuites];
 };
